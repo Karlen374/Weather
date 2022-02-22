f04dc69e1c23a96d0a742f46d3539f930e9e84d6
Karlen <svaryan01@inbox.ru>
2022-02-23 01:54:08 +0300

    useHttp

--- src/components/services/ConversionApi.js
+++ src/components/services/ConversionApi.js
@@ -1,23 +1,11 @@
-import { useCallback } from "react";
+
+import { useHttp } from "../../hooks/http.hook";
 
 const useConversionServices=()=>{
   const _apikey='d9e1c420-72c1-11ec-bb57-65e1d907cb11';
   const _apiBase='https://freecurrencyapi.net/api/v2/'
+  const {request}=useHttp()
   
-  const request=useCallback(async(url,method='GET',body=null,headers={'Content-Type':'application/json'})=>{
-
-    try{
-      const response=await fetch(url,{method,body,headers});
-      if (!response.ok){
-        throw new Error(`Could not fetch ${url} status: ${response.status}`)
-      }
-      const data=await response.json();
-      return data;
-    }catch(e){
-  
-      throw e;
-    }
-  },[])
 
   const getConvertValue=async(base_currency='USD')=>{
     const res= await request(`${_apiBase}latest?apikey=${_apikey}&base_currency=${base_currency}`)
--- src/components/services/WeatherApi.js
+++ src/components/services/WeatherApi.js
@@ -1,23 +1,11 @@
-import { useCallback } from "react";
+
+import {useHttp} from '../../hooks/http.hook'
 
 const useWeatherServices=()=>{
   const _apiBase = 'https://api.weatherapi.com/v1/';
   const  _apikey = 'key= 1ff90ee44b664ef785f22329220501 ';
-
-  const request=useCallback(async(url,method='GET',body=null,headers={'Content-Type':'application/json'})=>{
-
-    try{
-      const response=await fetch(url,{method,body,headers});
-      if (!response.ok){
-        throw new Error(`Could not fetch ${url} status: ${response.status}`)
-      }
-      const data=await response.json();
-      return data;
-    }catch(e){
+  const {request}=useHttp()
   
-      throw e;
-    }
-  },[])
   const  getWeatherByCity= async(city)=>{
     const res = await request(`${_apiBase}current.json?${_apikey} &q=${city}&aqi=no`)
     return _transformWeather(res);
@@ -38,14 +26,6 @@ const useWeatherServices=()=>{
     }
   }
   const _transformWeatherByDays=(res)=>{
-    // let arr=[res.forecast.forecastday[1].day.maxtemp_c,res.forecast.forecastday[2].day.maxtemp_c];
-    // console.log(arr)
-    // let arr=[];
-    // for(let i=0;i<2;i++){
-    //   arr[i]=res.forecast.forecastday[i+1].day.maxtemp_c;
-    // }
-    console.log(res);
-    console.log(`res=${res}`)
     return{
       name:res.location.name,
       country:res.location.country,
